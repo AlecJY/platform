783be6e9b452c82f5439235d18bad5180a2b7408
AlecJY <ae40515@yahoo.com.tw>
2020-03-11 14:26:28 +0800

    Modify flags and scripts to support vs2019

--- windows/build.cmd
+++ windows/build.cmd
@@ -5,8 +5,8 @@ rem Build the p8 platform library for Windows
 SETLOCAL
 
 SET MYDIR=%~dp0
-SET BUILDTYPE=Release
-SET VSVERSION=12
+SET BUILDTYPE=Debug
+SET VSVERSION=16
 SET INSTALLPATH=%MYDIR%..\build
 
 rmdir %MYDIR%..\build /s /q
@@ -15,4 +15,4 @@ for %%T in (amd64 x86) do (
   call %MYDIR%\build-lib.cmd %%T %BUILDTYPE% %VSVERSION% %INSTALLPATH%
 )
 
-rmdir %MYDIR%..\build\cmake /s /q
+rem rmdir %MYDIR%..\build\cmake /s /q
